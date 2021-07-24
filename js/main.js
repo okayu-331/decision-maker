6d47a8f759a8f1b39fe7fe70a22947bbd1a05675
okayu-331 <kuma.fuyuno@gmail.com>
2021-07-24 11:15:34 +0900

    add:アイテムを動的に生成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 {
   'use strict';
+
+  const items = [
+    {src: "img/drink_caramel.png", alt: "キャラメルドリンクの画像", result: "キャラメル"},
+    {src: "img/drink_chocolate.png", alt: "チョコレートドリンクの画像", result: "チョコレート"},
+    {src: "img/drink_strawberry.png", alt: "ストロベリードリンクの画像", result: "ストロベリー"},
+    {src: "img/drink_matcha.png", alt: "抹茶ドリンクの画像", result: "抹茶"},
+  ];
+
+  function createItems() {
+    const itemsContainer = document.getElementById('items-container');
+    items.forEach((item) => {
+      const list = document.createElement('li');
+      const imgElement = document.createElement('img');
+      list.className = 'item';
+      imgElement.src = item.src;
+      imgElement.alt = item.alt;
+      list.appendChild(imgElement);
+      itemsContainer.appendChild(list);
+    });
+  }
+
+  createItems();
 }
