78df1306240a96f21c814fe0c9b9cd1eea7b5253
okayu-331 <kuma.fuyuno@gmail.com>
2021-07-24 13:37:31 +0900

    add:抽選機能

--- js/main.js
+++ js/main.js
@@ -7,9 +7,26 @@
     {src: "img/drink_strawberry.png", alt: "ストロベリードリンクの画像", result: "ストロベリー"},
     {src: "img/drink_matcha.png", alt: "抹茶ドリンクの画像", result: "抹茶"},
   ];
+  const startBtn = document.getElementById('start-btn');
+  const itemsContainer = document.getElementById('items-container');
+
+  startBtn.addEventListener('click', (e) => {
+    if(e.target.className === 'disabled') {
+      return;
+    }
+    startBtn.className = 'disabled';
+    const styledItems = document.querySelectorAll('.picked, .unpicked');
+    if (styledItems.length !== 0) {
+      styledItems.forEach((item) => {
+        item.className = 'item';
+      });
+    }
+
+    startBtn.className = 'disabled';
+    setTimeout(lottery, 2000);
+  });
 
   function createItems() {
-    const itemsContainer = document.getElementById('items-container');
     items.forEach((item) => {
       const list = document.createElement('li');
       const imgElement = document.createElement('img');
@@ -19,6 +36,22 @@
       list.appendChild(imgElement);
       itemsContainer.appendChild(list);
     });
+  }
+
+  function lottery() {
+    const resultNum = Math.floor(Math.random() * items.length);
+    console.log(resultNum);
+
+    const itemLists = document.querySelectorAll('.item');
+    itemLists.forEach((item, index) => {
+      if (index === resultNum) {
+        item.classList.add('picked');
+      } else {
+        item.classList.add('unpicked');
+      }
+    })
+
+    startBtn.classList.remove('disabled');
   }
 
   createItems();
