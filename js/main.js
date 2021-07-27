6230d2a965876ca6c504cde4fdcadfc4af872b11
okayu-331 <kuma.fuyuno@gmail.com>
2021-07-28 00:51:05 +0900

    add:タブメニューで対象を切り替えて抽選

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 {
   'use strict';
 
-  const items = [
-    {src: "img/drink_caramel.png", alt: "キャラメルドリンクの画像", result: "キャラメル"},
-    {src: "img/drink_chocolate.png", alt: "チョコレートドリンクの画像", result: "チョコレート"},
-    {src: "img/drink_strawberry.png", alt: "ストロベリードリンクの画像", result: "ストロベリー"},
-    {src: "img/drink_matcha.png", alt: "抹茶ドリンクの画像", result: "抹茶"},
-  ];
+  const allItems = {
+    drinks: [
+      {src: "img/drink_caramel.png", alt: "キャラメルドリンクの画像", result: "キャラメル"},
+      {src: "img/drink_chocolate.png", alt: "チョコレートドリンクの画像", result: "チョコレート"},
+      {src: "img/drink_strawberry.png", alt: "ストロベリードリンクの画像", result: "ストロベリー"},
+      {src: "img/drink_matcha.png", alt: "抹茶ドリンクの画像", result: "抹茶"},
+    ],
+    foods: [
+      {src: "img/food_carbonara.png", alt: "キャラメルドリンクの画像", result: "カルボナーラ"},
+      {src: "img/food_meatsauce.png", alt: "チョコレートドリンクの画像", result: "ミートソース"},
+      {src: "img/food_neapolitan.png", alt: "ストロベリードリンクの画像", result: "ナポリタン"},
+      {src: "img/food_tarako.png", alt: "抹茶ドリンクの画像", result: "たらこ"},
+    ]
+  };
+
   const colors = [
     "linear-gradient(to right top, #f523a0, #db1946 , #ff468c, #ff8e4d)",
     "linear-gradient(to right top, #2ad5cd, #4c9af5 36%, #d295f1)",
@@ -19,14 +28,34 @@
   ];
   const startBtn = document.getElementById('start-btn');
   const progress = document.getElementById('progress');
+  const menuItems = document.querySelectorAll('.menu li');
   const itemsContainer = document.getElementById('items-container');
   const resultContainer = document.getElementById('result-container');
   const result = document.getElementById('result');
+  let category = document.querySelector('.active').dataset.id;
+  
+  menuItems.forEach(menu => {
+    menu.addEventListener('click', () => {
+      menuItems.forEach(menu => {
+        menu.classList.remove('active');
+      });
+      menu.classList.add('active');
+      category = document.querySelector('.active').dataset.id;
+
+      while(itemsContainer.firstChild){
+        itemsContainer.removeChild(itemsContainer.firstChild);
+      }
+      resultContainer.classList.remove('is-show');
+
+      createItems(allItems[category]);
+    });
+  })
 
   startBtn.addEventListener('click', (e) => {
     if(e.target.className === 'disabled') {
       return;
     }
+    let category = document.querySelector('.active').dataset.id;
     startBtn.className = 'disabled';
     const styledItems = document.querySelectorAll('.picked, .unpicked');
     if (styledItems.length !== 0) {
@@ -37,10 +66,10 @@
     }
 
     progress.classList.add('run');
-    setTimeout(lottery, 2000);
+    setTimeout(lottery, 2000, allItems[category]);
   });
 
-  function createItems() {
+  function createItems(items) {
     items.forEach((item) => {
       const list = document.createElement('li');
       const imgElement = document.createElement('img');
@@ -52,7 +81,7 @@
     });
   }
 
-  function lottery() {
+  function lottery(items) {
     const resultNum = Math.floor(Math.random() * items.length);
     const colorNum = Math.floor(Math.random() * colors.length);
 
@@ -72,5 +101,5 @@
     progress.classList.remove('run');
   }
 
-  createItems();
+  createItems(allItems[category]);
 }
